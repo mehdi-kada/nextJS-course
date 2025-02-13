aba7311d4a95cd930b4fab75a3dcb49bd0263025
mehdi-kada <mehdikada64@gmail.com>
2025-02-13 20:14:01 +0100

    fixed font importation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+{/* adding global styles to application */}
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts'; // Ensure 'fonts' file exists and is correctly imported
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
